b1f813acb8b80bac946a52ef7c96ad7f671b4958
HLong <info.longth@gmail.com>
2024-01-28 22:56:10 +0700

    change script setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
+cp .env.example .env
+docker-compose build app
+docker-compose up -d
 docker-compose exec app composer install
 docker-compose exec app php artisan migrate --force
 docker-compose exec app php artisan db:seed
+docker-compose exec app npm install
 docker-compose exec app npm run build
